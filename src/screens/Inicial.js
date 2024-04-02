436190032abb9c04340da1c1b5b0a61137fece59
Zimbo Sebastião <zimbo.sebastiao@global.ntt>
2024-04-02 01:20:06 -0300

    Update

--- src/screens/Inicial.js
+++ src/screens/Inicial.js
@@ -1,11 +1,16 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 
-import backgroundImage from "../../assets/images/login.png";
+import backgroundImage from "../../assets/images/inicio.png";
 import { Image } from "react-native";
 
 export default function Inicial({ navigation }) {
   return (
     <View style={estilos.container}>
+      <Image
+        source={backgroundImage}
+        resizeMode=""
+        style={{ ...estilos.background }}
+      />
       <View style={estilos.introducao}>
         <Text style={estilos.titulo}>React Native</Text>
         <Text style={estilos.subtitulo}>Autenticação com Firebase</Text>
@@ -27,32 +32,15 @@ export default function Inicial({ navigation }) {
 }
 
 const estilos = StyleSheet.create({
-  //   container: {
-  //     flex: 1,
-  //     backgroundColor: "#fff",
-  //     alignItems: "center",
-  //     justifyContent: "center",
-  //     padding: 16,
-  //   },
-  //   introducao: {
-  //     backgroundColor: "#4A235A",
-  //     width: "100%",
-  //     padding: 8,
-  //     borderRadius: 4,
-  //   },
-  //   titulo: {
-  //     textAlign: "center",
-  //     fontSize: 32,
-  //     color: "white",
-  //   },
-  //   subtitulo: {
-  //     textAlign: "center",
-  //     color: "white",
-  //   },
-  //   botoes: {
-  //     marginVertical: 8,
-  //     width: "100%",
-  //     flexDirection: "row",
-  //     justifyContent: "space-evenly",
-  //   },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  background: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    width: "auto",
+    height: "50%",
+  },
 });
